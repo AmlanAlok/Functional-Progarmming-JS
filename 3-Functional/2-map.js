03130e9b0de43cdeece11e34f6952f3d9c423f0e
Amlan Alok <amlanalok@gmail.com>
2022-07-06 14:37:03 -0500

    map implementation using for and reduce

--- 3-Functional/2-map.js
+++ 3-Functional/2-map.js
@@ -10,3 +10,22 @@ const doubleNumbers = numbers.map(double)
 
 console.log(numbers)
 console.log(doubleNumbers)
+
+
+const map1 = (arr, func) =>
+    arr.reduce((acc, x) => [
+        ...acc,
+        func(x)
+    ], [])
+
+const map2 = (arr, func) => {
+    let ans = []
+
+    for(let i = 0; i < arr.length; i++){
+        ans.push(func(arr[i]))
+    }
+    return ans
+}
+
+console.log(map1([1,2,3], x => x*2))
+console.log(map2([1,2,3], x => x*2))
