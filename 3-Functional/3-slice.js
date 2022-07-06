a6f72240cd287b5d63d553a1ef15c3d747742b66
Amlan Alok <amlanalok@gmail.com>
2022-07-05 20:57:08 -0500

    slice

---
+++ 3-Functional/3-slice.js
@@ -0,0 +1,8 @@
+/**
+ * node 3-Functional/3-slice.js
+ * slice returns a copy of the array and then you can manipulate this
+ */
+const nums = [1,2,3,4,5]
+
+const reverseNums = nums.slice().reverse()
+console.log(reverseNums)
